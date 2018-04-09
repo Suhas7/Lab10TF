d5d1142455f99d2f2f26eac833c7b03ebfe682eb
Suhas <suhasraja1@gmail.com>
2018-04-08 19:30:08 -0500

    just a lil more restructuring/organizing

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "/physics/Actors.hpp"
+#include "/audio/Sound.hpp"
+#include "/display/kentec.hpp"
+
+
+//Systick Handler
+void Systick_Handler(){
+	//detect collisions
+	for(int i =0; i<num_objects; i++){ //n^2 complexity
+		for(int j =i; j<num_objects;j++){
+			//if collision has happened, process it
+			if(checkCollision(objects[i], objects[j])) processCollision(objects[i], objects[j]);
+		}
+	}
+	for(int i =0; i<num_objects; i++) applyVel(i)
+	//decrement nonzero dodge values
+}
--- workpad.cpp
+++ workpad.cpp
@@ -1,24 +1,14 @@
-
-#include "../Sound.h"
 //TO DOs
-//DESIGN ASSETS
-	//insert default stages
-		const Wall* stage; //stage is defined as a collection of Walls, static
-	//insert sprite filepaths
-		ArcherSprites[4] = {'a','b','c','d'}
-		arrowSprites[6] = {'a','b','c','d','e','f'}
-	//insert sound bytes
-
-void playSound(float* s); //shift systick parameters accordingly
-//Systick Handler
-void Systick_Handler(){
-	//detect collisions
-	for(int i =0; i<num_objects; i++){ //n^2 complexity
-		for(int j =i; j<num_objects;j++){
-			//if collision has happened, process it
-			if(checkCollision(objects[i], objects[j])) processCollision(objects[i], objects[j]);
-		}
-	}
-	for(int i =0; i<num_objects; i++) applyVel(i)
-	//decrement nonzero dodge values
-}
+	//CALIBRATE PHYSICS
+	//DESIGN ASSETS
+		//insert default stages
+			const Wall* stage; //stage is defined as a collection of Walls, static
+		//insert sprite filepaths
+			ArcherSprites[4] = {'a','b','c','d'}
+			arrowSprites[6] = {'a','b','c','d','e','f'}
+		//insert sound bytes
+			Song* song;
+	//CONTROLLER STRUCTURE
+		//finish getJAngle
+		//design structure for button input
+		//relevant interrupts? e.g. jump edge triggered interrupt
