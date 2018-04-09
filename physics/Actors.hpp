712b4310eb5879ec414f4f9a5622f55a80b37b69
Suhas <suhasraja1@gmail.com>
2018-04-08 20:06:03 -0500

    split physics file structure across cpp/hpp

--- physics/Actors.hpp
+++ physics/Actors.hpp
@@ -4,8 +4,9 @@
 		#define xSIZE 320
 		#define ySIZE 240
 	//insert gravity weight
+		#define JUMPMAG 3
+		#define GRAVMAG 2
 	//insert arrow init velocities
-
 
 class ActiveObject{
 	//physics
@@ -13,19 +14,23 @@ class ActiveObject{
 	float colliderOffset;
 	float xPos;
 	float yPos;
+	float angle;
 	float xVel;
-	float yVal;
+	float yVel;
+	float rVel
 	float xAcc;
 	float yAcc;
 	float terminalX;
 	float terminalY;
-	//operationss
+
+	//operations
 	ActiveObject(int type);
 	applyVel();
 	applyForce(float x, float y);
 	halt();
 	haltX();
 	haltY();
+	haltR();
 }
 
 class Archer: public ActiveObject{
@@ -50,7 +55,7 @@ class Archer: public ActiveObject{
 		shoot(float angle);
 }
 
-class Arrow : public ActiveObject{
+class Arrow: public ActiveObject{
 	private:
 		int arrowType;
 	public:
@@ -64,46 +69,4 @@ class Wall{
 	float y1;
 	float x2;
 	float y2;
-}
-
-
-void checkCollision(ActiveObject& a, ActiveObject& b){ //check if collider radii cross
-	float xDist = (a->xPos-b->xPos)*(a->xPos-b->xPos);
-	float yDist = (a->yPos-b->yPos)*(a->yPos-b->yPos);
-	float threshold = (a->colliderRadius+b->colliderRadius)*(a->colliderRadius+b->colliderRadius);
-	if(threshold>=(xDist+yDist)) return true;
-	return false;
-}
-void processCollision(ActiveObject& a, ActiveObject& b){ //
-		//resolve collisions
-		//wall/arrow: arrow halts x,y
-		//wall/player: player halts both- key to wall jump, can wall hang
-		//arrow/player: 
-			//destroy player instance, iff player.dodge==0 && arrowspeed>=fatal
-			//else if dodge, give arrow
-			//else phase arrow
-			//according sound FX
-		//arrow/arrow: halt both, slight upward/rand horizontal speed
-		//player/player: halts along axist of collision
-}
-
-void ActiveObject::applyVel(){
-	this->xPos+=this->xVel/60;
-	this->xPos %= xSIZE;
-	this->yPos+=this->yVel/60;
-	this->yPos %= ySIZE;
-}
-
-void ActiveObject::applyForce(float x, float y){
-	this->xVel+=x;
-	this->xVel %= this->terminalX;
-	this->yVel+=y;
-	this->yVel %= this->terminalY;
-}
-
-void Archer::catch(Arrow& caughtArrow){
-	if(this->inventorySize==4){caughtArrow->halt();return;} //if inventory full, halt arrow and return
-	this->arrowInventory[inventorySize]=caughtArrow->arrowType; //else, add to inventory and destroy
-	delete caughtArrow;
-	playSound(sByte[4]);
 }
---
+++ physics/Physics.cpp
@@ -0,0 +1,67 @@
+//KEEP AN EYE OUT FOR GETTING STUCK ON WALLS
+
+//GENERAL PHYSICS
+	//MOVEMENT PROCESSING
+		void checkCollision(ActiveObject& a, ActiveObject& b){ //check if collider radii cross
+			float xDist = (a->xPos-b->xPos)*(a->xPos-b->xPos);
+			float yDist = (a->yPos-b->yPos)*(a->yPos-b->yPos);
+			float threshold = (a->colliderRadius+b->colliderRadius)*(a->colliderRadius+b->colliderRadius);
+			if(threshold>=(xDist+yDist)) return true;
+			return false;
+		}
+		void processCollision(ActiveObject& a, ActiveObject& b){ //
+				//resolve collisions
+				//wall/arrow: arrow halts x,y
+				//wall/player: player halts both- key to wall jump, can wall hang
+				//arrow/player: 
+					//destroy player instance, iff player.dodge==0 && arrowspeed>=fatal
+					//else if dodge, give arrow
+					//else phase arrow
+					//according sound FX
+				//arrow/arrow: halt both, slight upward/rand horizontal speed
+				//player/player: halts along axist of collision
+		}
+	//MOVEMENT APPLICATION
+		void ActiveObject::applyVel(){
+			this->xPos+=this->xVel/60;
+			this->xPos %= xSIZE;
+			this->yPos+=this->yVel/60;
+			this->yPos %= ySIZE;
+		}
+
+		void ActiveObject::applyForce(float x, float y){
+			this->xVel+=x;
+			this->xVel %= this->terminalX;
+			this->yVel+=y;
+			this->yVel %= this->terminalY;
+		}
+	//MOVEMENT ERASURE
+		void ActiveObject::haltX() this->xVel=0;
+		void ActiveObject::haltY() this->YVel=0;
+		void ActiveObject::haltR() this->rVel=0;
+		void ActiveObject::halt(){this->haltX();this->haltY();this->haltR();}
+
+//ARCHER BEHAVIOUR
+	void Archer::catch(Arrow& caughtArrow){
+		if(this->inventorySize==4){caughtArrow->halt();return;} //if inventory full, halt arrow and return
+		this->arrowInventory[inventorySize]=caughtArrow->arrowType; //else, add to inventory and destroy
+		delete caughtArrow;
+		playSound(sByte[4]);
+	}
+	void Archer::jump(){this->yVel=JUMPVEL}
+
+	//TO IMPLEMENT
+	void Archer::shoot(float angle){
+		Arrow shotArrow = new arrow(this->arrowInventory[0]);
+		shotArrow->angle=angle
+	}
+	void Archer::aim(float angle){
+		//while !duck || bBut hit
+		//get controller input
+		//set archer aim at new angle
+	}
+	
+	void Archer::dodge(float angle){
+		
+		
+	}
