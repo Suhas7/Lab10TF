236989a2f040cb2b1412c1acfeae97743e140e93
Suhas <suhasraja1@gmail.com>
2018-04-08 19:08:00 -0500

    cleaned up oop

---
+++ hardware/Controller.hpp
@@ -0,0 +1,29 @@
+int** portSettings = {{0,0,0},{0,0,0},{0,0,0},{0,0,0}};
+class Controller{private:
+  int ID;
+  SlidePot X;
+  SlidePot Y;
+  int aBut;
+  int bBut;
+  int jBut;
+  public:
+    Controller()
+
+}
+
+
+class SlidePot{ private:
+  uint32_t data;     // raw ADC value
+  int32_t flag;      // 0 if data is not valid, 1 if valid
+  uint32_t distance; // distance in 0.001cm
+// distance = (slope*data+offset)/4096
+  uint32_t slope;    // calibration coeffients
+  uint32_t offset;
+public:
+  SlidePot(uint32_t m, uint32_t b); // initialize slide pot
+  void Save(uint32_t n);        // save ADC, set semaphore
+  void Sync(void);              // wait for semaphore
+  uint32_t Convert(uint32_t n); // convert ADC to raw sample
+  uint32_t ADCsample(void);     // return last ADC sample value (0 to 4095)
+  uint32_t Distance(void);      // return last distance value (0 to 2000), 0.001cm
+};
--- workpad.cpp
+++ workpad.cpp
@@ -1,7 +1,20 @@
+
+#include "../Sound.h"
+//winSize
+#define xSIZE 320
+#define ySIZE 240
+#define gravMagnitude 2
+//insert arrow init velocities
+//DESIGN ASSETS
+	//insert default stages
+	//insert sprite filepaths
+	//insert sound bytes
+const Wall* stage; //stage is defined as a collection of Walls, static
 ArcherSprites[4] = {'a','b','c','d'}
 arrowSprites[6] = {'a','b','c','d','e','f'}
-//insert arrow init velocities
-const Wall* stage; //stage is defined as a collection of Walls, static
+
+//KEEP AN EYE OUT FOR UNITIZATION AND INTERNAL RESOLUTION
+
 
 class Wall{
 	float x1;
@@ -10,7 +23,8 @@ class Wall{
 	float y2;
 }
 
-class ActiveObject{ //make Arrow/Archer inherit?
+class ActiveObject{
+	//physics
 	float colliderRadius;
 	float colliderOffset;
 	float xPos;
@@ -21,74 +35,94 @@ class ActiveObject{ //make Arrow/Archer inherit?
 	float yAcc;
 	float terminalX;
 	float terminalY;
+	//operationss
 	ActiveObject(int type);
-}
+	applyVel();
+	applyForce(float x, float y);
+	halt();
+	haltX();
+	haltY();	}
 
-class Archer{
+class Archer: public ActiveObject{
 	private:
 		int playerID;
-		int* arrowInventory;
-		//physics
-		float colliderRadius;
-		float colliderOffset;
-		float xPos;
-		float yPos;
-		float xVel;
-		float yVal;
-		float xAcc;
-		float yAcc;
-		float terminalX;
-		float terminalY;
+		int inventorySize;
+		int[8] arrowInventory;
 		//status
-		float dodged; //framecount
+		int dodged; //framecount, 15fm?
 		bool grounded;
+		bool aiming;
 	public:
 		//constructor/destructors
 		Archer();
 		Archer(int PlayerNum)
 		~Archer();
-		//physics
-		halt();
-		haltX();
-		haltY();
-		applyForce(float x, float y);
 		//behaviors
-		catch(Arrow& caughtArrow);
+		catch(Arrow& caughtArrow); //done
 		jump();
 		dodge(float angle);
+		aim(float angle);
+		shoot(float angle);
 }
 
-class Arrow{
+class Arrow : public ActiveObject{
 	private:
 		int arrowType;
-		float colliderRadius;
-		float colliderOffset;
-		float xPos;
-		float yPos;
-		float xVel;
-		float yVal;
-		float xAcc;
-		float yAcc;
-		float terminalX;
-		float terminalY;
 	public:
 		//constructor/destructors
 		Arrow();
 		Arrow(int arrowtype);
 		~Arrow();
-		applyForce(float x, float y);
 }
 
 //Systick Handler
 void Systick_Handler(){
 	//detect collisions
-	//resolve collisions
-		//wall/arrow: arrow halts x,y
-		//wall/player: player halts both- key to wall jump, can wall hang
-		//arrow/player: destroy player instance, iff player.dodge==0, otherwise give arrow
-			//according sound FX
-		//arrow/arrow: halt x of both
-		//player/player: halts along axist of collision
-	//apply velocities to all players, objects
+	for(int i =0; i<num_objects; i++){ //n^2 complexity
+		for(int j =i; j<num_objects;j++) processCollision(objects[i], objects[j]);
+	}
+	for(int i =0; i<num_objects; i++) applyVel(i)
 	//decrement nonzero dodge values
 }
+void checkCollision(ActiveObject& a, ActiveObject& b){ //check if collider radii cross
+	float xDist = (a->xPos-b->xPos)*(a->xPos-b->xPos);
+	float yDist = (a->yPos-b->yPos)*(a->yPos-b->yPos);
+	float threshold = (a->colliderRadius+b->colliderRadius)*(a->colliderRadius+b->colliderRadius);
+	if(threshold>=(xDist+yDist)) return true;
+	return false;
+}
+void processCollision(ActiveObject& a, ActiveObject& b){ //
+		//resolve collisions
+		//wall/arrow: arrow halts x,y
+		//wall/player: player halts both- key to wall jump, can wall hang
+		//arrow/player: 
+			//destroy player instance, iff player.dodge==0 && arrowspeed>=fatal
+			//else if dodge, give arrow
+			//else phase arrow
+			//according sound FX
+		//arrow/arrow: halt both, slight upward/rand horizontal speed
+		//player/player: halts along axist of collision
+}
+
+void ActiveObject::applyVel(){
+	this->xPos+=this->xVel/60;
+	this->xPos %= xSIZE;
+	this->yPos+=this->yVel/60;
+	this->yPos %= ySIZE;
+}
+
+void ActiveObject::applyForce(float x, float y){
+	this->xVel+=x;
+	this->xVel %= this->terminalX;
+	this->yVel+=y;
+	this->yVel %= this->terminalY;
+}
+
+void Archer::catch(Arrow& caughtArrow){
+	if(this->inventorySize==4){caughtArrow->halt();return;} //if inventory full, halt arrow and return
+	this->arrowInventory[inventorySize]=caughtArrow->arrowType; //else, add to inventory and destroy
+	caughtArrow::~Arrow();
+	playSound(sByte[4]);
+}
+
+void playSound(float* s); //shift systick parameters accordingly
